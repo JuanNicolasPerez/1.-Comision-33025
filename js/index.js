b0d5f2cdfeceee9995ed8fc2babc2f3da8e9f4dc
Juan Nicolas Perez <nicko_speed2@gmail.com>
2022-08-25 15:28:35 -0300

    Aplicando Bootstrap

--- js/index.js
+++
@@ -1,6 +0,0 @@
-const toggle = document.querySelector(".toggle")
-const navmenu = document.querySelector(".nav_menu")
-
-toggle.addEventListener("click", () =>{
-    navmenu.classList.toggle("nav_menu_visible")
-})
